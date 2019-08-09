43e66908cf691789e1b2c560a494510d81afe383
matei.eduard <eduardmatei0@yahoo.com>
2019-08-09 18:04:44 +0300

    build

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controller/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -64,6 +65,9 @@ app.use(
     ]
   })
 );
+
+// compress text sent to clients
+app.use(compression());
 
 // Test middleware
 app.use((req, res, next) => {
